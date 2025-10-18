e660fdced2bcc1cd107e98c7e61a85b02c62a4ec
umut.eth <u.satir2021@gtu.edu.tr>
2025-10-18 20:05:56 +0200

    logo added

--- packages/nextjs/components/Navbar.tsx
+++ packages/nextjs/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { NesWalletConnectButton } from "./NesWalletConnectButton";
 import SimpleENSSearchDialog from "./SimpleENSSearchDialog";
 
@@ -6,7 +7,7 @@ export const Navbar = () => {
     <div className="navbar bg-transparent fixed top-0 left-0 right-0 z-50 px-5 pt-5">
       <div className="navbar-start">
         <div className="dropdown"></div>
-        <a className="btn btn-ghost text-xl">BitBrawlers</a>
+        <Image src="/logo.png" alt="BitBrawlers" width={64} height={64} />
       </div>
       <div className="navbar-end gap-2 flex">
         <SimpleENSSearchDialog />
