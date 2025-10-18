546718aa09f3b18e29480d437ae7c3cc5e752454
umut.eth <u.satir2021@gtu.edu.tr>
2025-10-18 12:42:05 +0200

    nes.css added

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
+import "nes.css/css/nes.min.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
